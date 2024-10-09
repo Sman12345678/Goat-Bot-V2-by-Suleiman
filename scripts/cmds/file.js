f6e52222efa084ef02277c8fbe9ba14ae23aee31
Suleiman <Suleimanayomide716@gmail.com>
2024-10-09 13:45:09 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
+ const fs = require('fs');
 
 module.exports = {
   config: {
     name: "file",
     version: "1.0",
-    author: "Suleiman",
+    author: "Raphael scholar",
     countDown: 1,
     role: 2,
     shortDescription: "File",
@@ -16,7 +16,7 @@ module.exports = {
   onStart: async function ({ message, args, api, event }) {
     const subash = ["61553565805276"];
     if (!subash.includes(event.senderID)) {
-      return api.sendMessage("🚫 | You don't have the access to such command!", event.threadID, event.messageID);
+      return api.sendMessage("❌ | You don't have the access!", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
